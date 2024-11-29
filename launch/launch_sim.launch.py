1c29289212f7cc2a8d5037b9792f6eeb25cd257a
Kim <k32hoang@uwaterloo.ca>
2024-11-28 21:56:39 -0500

    fix launch file and add driving

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -7,37 +7,35 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+
 from launch_ros.actions import Node
 
 
 
 def generate_launch_description():
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='navbot' #<--- CHANGE ME
+    package_name='navbot' 
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
     )
-
+    
     default_world = os.path.join(
         get_package_share_directory(package_name),
         'worlds',
         'empty.world'
-    )
-
+        )    
+    
     world = LaunchConfiguration('world')
 
     world_arg = DeclareLaunchArgument(
         'world',
         default_value=default_world,
         description='World to load'
-    )
+        )
+
     # Include the Gazebo launch file, provided by the ros_gz_sim package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -48,28 +46,27 @@ def generate_launch_description():
     # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-name', 'navbot',
+                                   '-name', 'navigation_bot',
                                    '-z', '0.1'],
                         output='screen')
 
-
-    bridge_params = os.path.join(get_package_share_directory(package_name), 'config', 'gz_bridge.yaml')
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
     ros_gz_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
         arguments=[
             '--ros-args',
             '-p',
             f'config_file:={bridge_params}',
         ]
     )
-    
+
     ros_gz_image_bridge = Node(
-        package='ros_gz_image',
-        executable='image_bridge',
+        package="ros_gz_image",
+        executable="image_bridge",
         arguments=["/camera/image_raw"]
     )
-    # Launch them all!
+
     return LaunchDescription([
         rsp,
         world_arg,
@@ -78,3 +75,4 @@ def generate_launch_description():
         ros_gz_bridge,
         ros_gz_image_bridge
     ])
+    
